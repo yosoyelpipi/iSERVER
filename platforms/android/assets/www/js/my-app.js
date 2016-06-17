5462e5fb5b5c8de9a0244c03351146e5241f7d15
Leo Condori <lcondori@gmail.com>
2016-06-17 12:43:57 -0300

    2.1

--- platforms/android/assets/www/js/my-app.js
+++ platforms/android/assets/www/js/my-app.js
@@ -30,7 +30,24 @@ $$(document).on('deviceready', function() {
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
 myApp.onPageInit('about', function (page) {
     // Do something here for "about" page
+function testNow(){
+    myApp.alert('El servidor iSERVER está caído.');
+    //$("#estado").show();
+    var port = $("#puerto").val();
+    $("#log").html('<div align="center" class="col-25">Conectando...<br><span style="width:42px; height:42px" class="preloader"></span></div>');		
+    $.getJSON("http://leocondori.com.ar/app/iserver/testconexion.php", {puerto: port}, ItsTestResul, "json");
+}
 
+function ItsTestResul(Response){
+    $("#log").html('');
+    if (Response.Resultado == 0){
+        myApp.alert(Response.Mensaje + ' con el puerto ' + Response.Puerto);
+        $("#log").append(Response.Mensaje + ' con el puerto '+ Response.Puerto + ' <br>');
+    }else{
+        myApp.alert(Response.Msg + ' número de error ' + Response.MsgNo);
+        $("#log").append(Response.Mensaje + ' con el puerto '+ Response.Puerto + ' <br>');
+    }
+}
 })
 
 // Option 2. Using one 'pageInit' event handler for all pages:
@@ -50,23 +67,3 @@ $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
     myApp.alert('Here comes About page');
 })
-
-
-function testNow(){
-    myApp.alert('El servidor iSERVER está caído.');
-    //$("#estado").show();
-    var port = $("#puerto").val();
-    $("#log").html('<div align="center" class="col-25">Conectando...<br><span style="width:42px; height:42px" class="preloader"></span></div>');		
-    $.getJSON("http://leocondori.com.ar/app/iserver/testconexion.php", {puerto: port}, ItsTestResul, "json");
-}
-
-function ItsTestResul(Response){
-    $("#log").html('');
-    if (Response.Resultado == 0){
-        myApp.alert(Response.Mensaje + ' con el puerto ' + Response.Puerto);
-        $("#log").append(Response.Mensaje + ' con el puerto '+ Response.Puerto + ' <br>');
-    }else{
-        myApp.alert(Response.Msg + ' número de error ' + Response.MsgNo);
-        $("#log").append(Response.Mensaje + ' con el puerto '+ Response.Puerto + ' <br>');
-    }
-}
