d30ba65255acbc0c0024ecfe5948dec6432e74c8
Leo Condori <lcondori@gmail.com>
2016-08-23 22:22:10 -0300

    7.1

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $link=mysqli_connect("localhost", "maxiroa_admin", "itris4583","maxiroa_reloj");
-
-    if($link){
-        $MsgMySql = 'Conectado al servidor MySql.';
-    }else{
-        $MsgMySql = mysqli_connect_error();
-    }
-?>
