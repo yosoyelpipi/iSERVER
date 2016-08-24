9f011ca1f2bf354fc059f1dfdca7fa0d33526167
Leo Condori <lcondori@gmail.com>
2016-08-23 22:08:55 -0300

    6.9

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,71 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-media/www/MediaError.js",
+        "id": "cordova-plugin-media.MediaError",
+        "clobbers": [
+            "window.MediaError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media/www/Media.js",
+        "id": "cordova-plugin-media.Media",
+        "clobbers": [
+            "window.Media"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.battery-status/www/battery.js",
+        "id": "org.apache.cordova.battery-status.battery",
+        "clobbers": [
+            "navigator.battery"
+        ]
+    },
+    {
+        "file": "plugins/com.eficid.cordova.plugin.rfidscanner/www/rfidscanner.js",
+        "id": "com.eficid.cordova.plugin.rfidscanner.RfidScanner",
+        "clobbers": [
+            "window.rfidscanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.0.3",
+    "cordova-plugin-device": "1.1.2",
+    "cordova-plugin-geolocation": "2.2.0",
+    "cordova-plugin-media": "1.0.1",
+    "cordova-plugin-statusbar": "1.0.1",
+    "cordova-plugin-whitelist": "1.2.2",
+    "org.apache.cordova.battery-status": "0.2.12",
+    "com.eficid.cordova.plugin.rfidscanner": "0.1.0"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/www/in-out.php
@@ -0,0 +1,3 @@
+<?php
+echo '{"operacion": '.$_GET['operacion'].', "fecha":"'.$_GET['fecha'].'", "horas":"'.$_GET['horas'].'","lat":"'.$_GET['lat'].'","lon":"'.$_GET['lon'].'","deviceuuid":"'.$_GET['deviceuuid'].'", "host":"'.$_GET['host'].'","code":"'.$_GET['code'].'" }';
+?>
